6c527274c1f89d2b376f4fb7c92e160d6c21f110
MKZKJSNDDN <145093246+DittomafiaOP@users.noreply.github.com>
2023-12-03 15:20:21 +0530

    Update echo.py

--- AltBots/modules/echo.py
+++ AltBots/modules/echo.py
@@ -27,11 +27,11 @@ async def echo(event):
             user_id = reply_msg.sender_id
 
             if user_id in ALTRON:
-                await event.reply("ɴᴏ, ᴛʜɪꜱ ɢᴜʏ ɪꜱ ᴀʟᴛʀᴏɴ'ꜱ ᴏᴡɴᴇʀ.")
+                await event.reply(" ABE OWENER HAI WO BOT KA BSDK.")
             elif user_id == OWNER_ID:
-                await event.reply("ɴᴏ, ᴛʜɪꜱ ɢᴜʏ ɪꜱ ᴏᴡɴᴇʀ ᴏꜰ ᴛʜᴇꜱᴇ ʙᴏᴛꜱ.")
+                await event.reply("ABE OWNER HAI WO BOT KA BSDK.")
             elif user_id in SUDO_USERS:
-                await event.reply("ɴᴏ, ᴛʜɪꜱ ɢᴜʏ ɪꜱ ᴀ ꜱᴜᴅᴏ ᴜꜱᴇʀ.")
+                await event.reply("ABE OWNER HAI WO BOT KA BSDK.")
             else:
                 try:
                     alt = Get(base64.b64decode('QFRoZUFsdHJvbg=='))
